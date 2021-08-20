04348a7d89b1dce096945a890cde6da50dc4a60c
胡鸣 <9185847+huming3272@user.noreply.gitee.com>
2021-08-20 22:44:10 +0800

    打包了一下

---
+++ dist/precache-manifest.cf2ed06aa341fcaab89ed4d1a87565eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6087b5922214679ef797",
+    "url": "/easy-tally-book/css/app.ce81de02.css"
+  },
+  {
+    "revision": "25e3c2d8334eb285cd11",
+    "url": "/easy-tally-book/css/chunk-vendors.caacd1e0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/easy-tally-book/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/easy-tally-book/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "3795415c86746d24d802dbeedd079396",
+    "url": "/easy-tally-book/index.html"
+  },
+  {
+    "revision": "bc97ea2759f455698897",
+    "url": "/easy-tally-book/js/about.4bec02ab.js"
+  },
+  {
+    "revision": "6087b5922214679ef797",
+    "url": "/easy-tally-book/js/app.bcb4b09f.js"
+  },
+  {
+    "revision": "25e3c2d8334eb285cd11",
+    "url": "/easy-tally-book/js/chunk-vendors.a6bb7c0d.js"
+  },
+  {
+    "revision": "70ecdce57616d4c4e5e059e3853de8d4",
+    "url": "/easy-tally-book/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easy-tally-book/robots.txt"
+  }
+]);
